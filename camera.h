08dcfc2133fe8f3eaea69b0a36887db1b0b9c33b
leonmavr <0xleo.git@gmail.com>
2024-12-18 18:08:51 +0200

    init

---
+++ camera.h
@@ -0,0 +1,49 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct {
+  float x, y, z;
+} vec3f_t;
+typedef struct { int x, y; } vec2i_t;
+typedef struct { uint8_t x, y, z; } vec3u_t;
+
+/** Pinhole camera */
+typedef struct {
+  float cx, cy;           // camera's center of perspective
+  float f;                // focal length
+  struct {
+    float x0, y0, x1, y1;
+    int width, height;    // for easier referencing
+  } boundary;             // projection plane boundaries
+  vec2i_t (*project)(float x, float y, float z, bool *is_visible);
+} camera_t;
+
+typedef struct {
+  camera_t camera;
+  float **dbuffer;        // depth buffer - depth of each point
+  uint32_t **pbuffer;     // color buffer - color of each point
+  void (*init)(float cx, float cy, float f, float fovx_deg, float fovy_deg);
+} scene_t;
+
+typedef struct {
+  vec3f_t origin;
+  float rad;
+  vec3u_t color;
+} sphere_t;
+
+extern scene_t scene;
+
+sphere_t sphere_make(float x0, float y0, float z0, float rad, uint8_t r, uint8_t g, uint8_t b);
+void pbuffer_write(int x, int y, uint8_t r, uint8_t g, uint8_t b);
+
+void sphere_write(sphere_t* sphere);
+void pbuffer_save(const char* filename);
+bool dbuffer_write(int x, int y, float dist);
+
+void scene_init(float cx, float cy, float f, float fovx_deg, float fovy_deg);
+void buffer_free();
+
+#endif // CAMERA_H
---
+++ main.c
@@ -0,0 +1,17 @@
+#include "camera.h"
+#include <stdio.h>
+
+int main() {
+  printf("start\n");
+  scene_init(0, 0, -300, 70, 70);
+  sphere_t spheres[3];
+  spheres[0] = sphere_make(0, 25, 600, 100, 0, 0, 200);
+  spheres[1] = sphere_make(100, 50, 600, 100, 200, 0, 0);
+  spheres[2] = sphere_make(-50, 100, 600, 150, 0, 200, 50);
+  for (int i = 0; i < sizeof(spheres)/sizeof(spheres[0]); ++i) {
+    sphere_write(&spheres[i]);
+  }
+  pbuffer_save("output.ppm");
+  buffer_free();
+  return 0;
+}
